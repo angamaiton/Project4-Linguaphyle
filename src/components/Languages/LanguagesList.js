28c3a6322db0b7024c23516d2024e01fd5b252d1
Kjartan Angamaiton <ofurmadur@icloud.com>
2017-01-18 14:13:31 -0600

    Revert "the fucking router is finally working, YAY"
    This reverts commit e2dfd16013f3d19ea8931347dd52d9eef69c62ba.
    
    : Your branch is ahead of 'origin/master' by 1 commit.

--- src/components/Languages/LanguagesList.js
+++ src/components/Languages/LanguagesList.js
@@ -1,17 +1,20 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
-import { Col, Nav, NavItem } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 export default class LanguagesList extends Component {
-  // static propTypes = {};
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    languages: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })).isRequired,
+  };
 
   render() {
     return (
-      <Col md={4}>
-        <Nav bsStyle="pills" stacked activeKey={1}>
-          <NavItem eventKey={1}><Link to="/languages/1">Language 1</Link></NavItem>
-          <NavItem eventKey={2}><Link to="/languages/2">Language 2</Link></NavItem>
-        </Nav>
+      <Col>
+        {this.props.languages.map((lang) => (<Link key={lang.id} className={s.link} to={`/languages/${lang.id}`}>{lang.name}</Link>))}
       </Col>
     );
   }
--- src/components/Languages/index.js
+++ src/components/Languages/index.js
@@ -1,9 +1,7 @@
 import React, { PropTypes, Component } from 'react';
-import { Col, PageHeader } from 'react-bootstrap';
+import { Col, PageHeader, FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 import cx from 'classnames';
 
-import LanguageSearchForm from './LanguageSearchForm';
-import LanguagesList from './LanguagesList';
 import './style.css';
 
 export default class Languages extends Component {
@@ -11,17 +9,45 @@ export default class Languages extends Component {
   // static defaultProps = {}
   // state = {}
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    }
+  }
+
+  getValidationState = () => {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+
   render() {
     const { className, ...props } = this.props;
     return (
       <div className={cx('Languages', className)} {...props}>
-        <Col xs={12} md={12}>
-          <PageHeader>Languages</PageHeader>
-          <Col xs={12} md={8}>
-            <LanguageSearchForm />
-          </Col>
-          <LanguagesList />
-          {this.props.children}
+        <Col xs={12} md={8}>
+          <PageHeader>Hello</PageHeader>
+          <form>
+            <FormGroup
+              controlId="formBasicText"
+              validationState={this.getValidationState()}>
+              <ControlLabel>Working example with validation</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Enter text"
+              />
+              <FormControl.Feedback />
+              <HelpBlock>Validation is based on string length.</HelpBlock>
+            </FormGroup>
+          </form>
         </Col>
       </div>
     );
